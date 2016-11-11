0ffa50de33125dd77db41d0c33c31376288918bd
Beljaev_Pavel <beljaev.pavel.kr@gmail.com>
2016-11-11 11:25:25 +0100

    end

---
+++ src/main/resources/scripts/PricingEngineImpl.groovy
@@ -0,0 +1,12 @@
+import com.pesahlavan.pizza.Order;
+import com.pesahlavan.pizza.Pizza;
+import com.pesahlavan.pizza.PricingEngine;
+import java.io.Serializable;
+
+class PricingEngineImpl implements PricingEngine, Serializable {
+  public float calculateOrderTotal(Order order) {
+    print "IN GROOVY";
+  
+    retun 99.99;
+  }
+}
